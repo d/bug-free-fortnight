d5a9feb4d87284f7fea320a6987accf79926f405
Jesse Zhang and Marc Spehlmann <marc.spehlmann@gmail.com>
2016-08-17 12:16:46 -0700

    Adaptive concurrency

--- streamline-master/build_the_universe.bash
+++ streamline-master/build_the_universe.bash
@@ -20,21 +20,21 @@ build_the_universe() {
 
 	cd /build/gp-xerces
 	/workspace/gp-xerces/configure --prefix ${prefix}
-	make -j8 install
+	make -j$(nproc) install
 
 	cd /build/gpos
 	cmake -DCMAKE_INSTALL_PREFIX=${prefix} /workspace/gpos
-	make -j8 install
+	make -j$(nproc) install
 
 	cd /build/gporca
 	cmake -DCMAKE_INSTALL_PREFIX=${prefix} /workspace/gporca
-	make -j8 install
+	make -j$(nproc) install
 
 	cd /build
 	git clone /workspace/gpdb
 	cd gpdb
 	./configure --enable-orca --enable-mapreduce --with-perl --with-libxml --with-python --disable-gpfdist --prefix=${prefix} --with-includes=${prefix}/include --with-libs=${prefix}/lib
-	make -j8 install
+	make -j$(nproc) install
 }
 
 make_cluster() {
