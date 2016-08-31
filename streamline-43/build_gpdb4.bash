19b36e6ee9c7470340926db154b09d419953ca9d
Jesse Zhang <jzhang@pivotal.io>
2016-08-31 00:35:56 -0700

    Make cluster in 4.3

--- streamline-43/build_gpdb4.bash
+++ streamline-43/build_gpdb4.bash
@@ -4,8 +4,9 @@ set -e -u -o pipefail
 set -x
 
 _main() {
-	inject_orca
-	build_gpdb4
+	time inject_orca
+	time build_gpdb4
+	time make_cluster
 }
 
 inject_orca() {
@@ -13,11 +14,20 @@ inject_orca() {
 }
 
 build_gpdb4() {
-	pushd /build/gpdb4/gpAux
 	: ${LD_LIBRARY_PATH:=}
-	time (
+	(
+	pushd /build/gpdb4/gpAux
 	source /opt/gcc_env.sh
 	env IVY_HOME=/opt/releng/ivy_home make BLD_CC='ccache gcc' rhel5_x86_64_CXX='ccache g++' GPROOT=/build/install PARALLEL_BUILD=1 dist
+	)
+}
+
+make_cluster() {
+	: ${LD_LIBRARY_PATH:=}
+	(
+	pushd /build/gpdb4/gpAux
+	source /build/install/greenplum-db-devel/greenplum_path.sh
+	make -C /build/gpdb4/gpAux/gpdemo
 	)
 }
 
