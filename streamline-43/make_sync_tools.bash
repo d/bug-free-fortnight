d907d46173c46f40db42139f92681994867c8824
Jesse Zhang <jzhang@pivotal.io>
2016-08-31 23:43:29 -0700

    Push chdir into subshell and use shared clone

--- streamline-43/make_sync_tools.bash
+++ streamline-43/make_sync_tools.bash
@@ -9,19 +9,18 @@ _main() {
 }
 
 make_sync_tools() {
-	pushd /build/gpdb4/gpAux
 	: ${LD_LIBRARY_PATH:=}
 	(
 	source /opt/gcc_env.sh
+	pushd /build/gpdb4/gpAux
 	make sync_tools
 	ln -svf /build/gpdb4/gpAux/ext/rhel5_x86_64/python-2.6.2 /opt
 	)
-	popd
 }
 
 clone_gpdb4() {
 	if [[ ! -e /build/gpdb4 ]]; then
-		git clone /workspace/gpdb4 /build/gpdb4
+		git clone --shared /workspace/gpdb4 /build/gpdb4
 	fi
 	pushd /build/gpdb4
 	rsync -r /workspace/gpdb4/.git/modules .git
