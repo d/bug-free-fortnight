7b7f34f1ddcfd034b1af25fbf6cc6885d9b5d706
Jesse Zhang <sbjesse@gmail.com>
2016-09-08 09:22:23 -0700

    Better editor support for shellcheck
    If you run `./test.bash`, it gives you a very human friendly output.
    
    If you are using VIM, run this before check in / sending PR:
    ```
    cexpr system('./test.bash -f gcc')
    ```
    And then use `:cc`, `:cn`, `:cp` to jump to errors

--- test.bash
+++ test.bash
@@ -3,7 +3,7 @@
 set -e -u -o pipefail
 
 _main() {
-	find . -name "*.bash" -exec shellcheck --exclude SC2164 --exclude SC2064 --shell bash {} +
+	find . -name "*.bash" -exec shellcheck "$@" --exclude SC2164 --exclude SC2064 --shell bash {} +
 }
 
 _main "$@"
