3b9f9dd9b5e71c12731349fe48f197576852c0d4
Jesse Zhang <sbjesse@gmail.com>
2016-12-28 10:20:44 +0800

    Extract configure flags and env

--- streamline-master/build_gpdb.bash
+++ streamline-master/build_gpdb.bash
@@ -19,15 +19,45 @@ build_gpdb() {
 	local prefix
 	prefix=$1
 
+	local -a CONFIGURE_ENV
+	CONFIGURE_ENV=(
+	CXX='ccache c++'
+	CC='ccache cc'
+	)
+
+	local -a CONFIGURE_FLAGS=(
+	--enable-orca
+	--enable-mapreduce
+	--with-perl
+	--with-libxml
+	--with-python
+	--enable-gpfdist
+	--prefix="${prefix}"
+	--with-includes="${prefix}/include"
+	--with-libs="${prefix}/lib"
+	)
+
+	build_gpdb_impl "${prefix}" CONFIGURE_ENV CONFIGURE_FLAGS
+}
+
+build_gpdb_impl() {
+	local prefix
+	prefix=$1
+	local -r configure_env_var="$2[@]"
+	local -r configure_flags_var="$3[@]"
+
 	cd /build
 	if [[ ! -e /build/gpdb ]]; then
 		git clone --shared /workspace/gpdb
 	fi
+	local -ra CONFIGURE_ENV=("${!configure_env_var}")
+	local -ra CONFIGURE_FLAGS=("${!configure_flags_var}")
+
 	cd gpdb
 	env \
-		CXX='ccache c++' \
-		CC='ccache cc' \
-		./configure --enable-orca --enable-mapreduce --with-perl --with-libxml --with-python --enable-gpfdist --prefix="${prefix}" --with-includes="${prefix}"/include --with-libs="${prefix}"/lib
+		"${CONFIGURE_ENV[@]}" \
+		./configure \
+		"${CONFIGURE_FLAGS[@]}"
 	make CXX='ccache c++' -j"$(nproc)" install
 
 }
