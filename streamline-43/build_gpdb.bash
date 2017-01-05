c2c47eb0e120716ae8d417fb8c2aa68825acd7ca
Jesse Zhang <sbjesse@gmail.com>
2017-01-05 05:47:28 +0000

    --enable-debug works with GPDB 4.3
    Note this only builds the postgres executable with debug symbols.
    
    This also addresses #6.

--- streamline-43/build_gpdb.bash
+++ streamline-43/build_gpdb.bash
@@ -11,9 +11,31 @@ _main() {
 	local prefix
 	prefix=/build/install
 
+	local build_mode
+	parse_args "$@"
+
 	time inject_orca
 	time build_gpdb4 "${prefix}"
 	time make_cluster "${prefix}"
+}
+
+parse_args() {
+	local args=("$@")
+	build_mode=opt
+
+	local opt
+	local OPTIND
+	while getopts :d opt "${args[@]+${args[@]}}" ; do
+		case "${opt}" in
+			d)
+				build_mode=debug
+				;;
+			*)
+				echo >&2 Unknown flag
+				return 1
+				;;
+		esac
+	done
 }
 
 inject_orca() {
@@ -33,18 +55,32 @@ build_gpdb4() {
 	# shellcheck disable=SC1091
 	source /opt/gcc_env.sh
 
+	local target
+	case "${build_mode}" in
+		debug)
+			target=devel
+			;;
+		*)
+			target=dist
+			;;
+	esac
+
 	local max_load
 	max_load=$(( $(ncpu) * 3 / 2))
+
+	local -a MAKEVARS=(
+	INSTLOC="${prefix}"
+	BLD_CC='ccache gcc'
+	rhel5_x86_64_CXX='ccache g++'
+	rhel6_x86_64_CXX='ccache g++'
+	GPROOT=/build/install
+	PARALLEL_BUILD=1
+	parallelexec_maxlimit=${max_load}
+	)
 	env IVY_HOME=/opt/releng/ivy_home \
 		make \
-		INSTLOC="${prefix}" \
-		BLD_CC='ccache gcc' \
-		rhel5_x86_64_CXX='ccache g++' \
-		rhel6_x86_64_CXX='ccache g++' \
-		GPROOT=/build/install \
-		PARALLEL_BUILD=1 \
-		parallelexec_maxlimit=${max_load} \
-		dist
+		"${MAKEVARS[@]}" \
+		"${target}"
 	)
 }
 
