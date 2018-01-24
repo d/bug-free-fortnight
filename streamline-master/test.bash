60b294d66e7fcc1903e4198de14043554d47a1f8
Jesse Zhang <sbjesse@gmail.com>
2018-01-24 11:21:15 -0800

    Switch to Ubuntu rolling (currently 17.10 artful)
    17.04 zesty was an awkward short-living release (like all non-LTS
    rolling releases). It was already out of the "rolling" designation by
    the time we chose it. And we only chose it because of new compiler
    warnings from GCC 7. Now that these have all been fixed, let's keep
    rolling forward! [Fixes #39]
    
    This commit also integrates testing of the image right into the process
    of "docker build": no more excuses for not running tests! [Closes #38]

---
+++ streamline-master/test.bash
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+set -x
+
+_main() {
+	it_has_locale en_US.utf8
+
+	it_has_gcc
+
+	it_has_libc_objects
+
+	it_has_working_cxx
+
+	it_has_modern_cmake
+
+	it_has_modern_ccache
+
+	it_has_ninja_17
+
+	it_has_executables
+
+	it_has_python_modules_visible_to_every_user
+}
+
+it_has_locale() {
+	local -r locale_name=$1
+	locale -a | grep -F "${locale_name}"
+}
+
+it_has_gcc() {
+	type -p gcc
+	type -p g++
+	type -p cc
+	type -p c++
+}
+
+it_has_libc_objects() {
+	(
+	set -e
+	pushd "$(mktemp -d -t simple_compilation.XXX)"
+	cat > hello.c <<HELLO
+#include <string.h>
+int main() { return 0; }
+HELLO
+	gcc -D_GNU_SOURCE -o hello hello.c
+	./hello
+	)
+}
+
+it_has_working_cxx() {
+	(
+	pushd "$(mktemp -d -t simple_cxx.XXX)"
+	cat > hello.cc <<HELLO
+#include <iostream>
+int main() { std::cout << 1ul << '\\n'; }
+HELLO
+	c++ -O -o hello hello.cc
+	./hello
+	)
+}
+
+it_has_modern_cmake() {
+	local cmake_version
+	readonly cmake_version=$(cmake --version 2>/dev/null | sed 's/[^0-9]\+/ /g')
+	local cmake_version_check
+	readonly cmake_version_check=$(mktemp -t cmake_version_check.XXX.awk)
+
+	cat > "${cmake_version_check}" << 'CMAKE_VERSION_CHECK'
+	{
+		if ($1 > 3 || ($1 == 3 && $2 >= 6))
+			exit 0;
+		else
+			exit 1;
+	}
+CMAKE_VERSION_CHECK
+
+	echo "${cmake_version}" | awk "$(< "${cmake_version_check}")"
+}
+
+it_has_modern_ccache() {
+	# ccache 2.x only supported short options
+	ccache --version
+}
+
+it_has_ninja_17() {
+	[[ "$(ninja --version)" = 1.7* ]]
+}
+
+it_has_executables() {
+	local EXECUTABLES=(
+		vim
+		make
+		patch
+		bzip2
+		unzip
+		pigz
+		xz
+		wget
+		ip
+	)
+	for executable in "${EXECUTABLES[@]}"; do
+		type -p "${executable}"
+	done
+}
+
+it_has_python_modules_visible_to_every_user() {
+	local -a PYTHON_MODULES=(
+	psutil
+	lockfile
+	paramiko
+	epydoc
+	)
+
+	local module
+	for module in "${PYTHON_MODULES[@]}"; do
+		python -c "import ${module}"
+	done
+}
+
+_main "$@"
