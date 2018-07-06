b31fad47646f7139f357c7d82ced6d17274de6c3
Jesse Zhang <sbjesse@gmail.com>
2018-07-06 16:02:22 -0700

    Relax ninja version check to 1.7+
    Instead of strictly 1.7

--- streamline-master/test.bash
+++ streamline-master/test.bash
@@ -62,20 +62,31 @@ HELLO
 
 it_has_modern_cmake() {
 	local cmake_version
-	readonly cmake_version=$(cmake --version 2>/dev/null | sed 's/[^0-9]\+/ /g')
-	local cmake_version_check
-	readonly cmake_version_check=$(mktemp -t cmake_version_check.XXX.awk)
+	readonly cmake_version=$(cmake --version 2>/dev/null)
 
-	cat > "${cmake_version_check}" << 'CMAKE_VERSION_CHECK'
+	check_version_major_minor "${cmake_version}" 3 6
+}
+
+check_version_major_minor() {
+	local version
+	readonly version=$(sed 's/[^0-9]\+/ /g' <<< "$1")
+	local major minor
+	readonly major=$2
+	readonly minor=$3
+
+	local version_check_awk_script
+	readonly version_check_awk_script=$(mktemp -t version_check.XXX.awk)
+
+	cat > "${version_check_awk_script}" << CMAKE_VERSION_CHECK
 	{
-		if ($1 > 3 || ($1 == 3 && $2 >= 6))
+		if (\$1 > ${major} || (\$1 == ${major} && \$2 >= ${minor}))
 			exit 0;
 		else
 			exit 1;
 	}
 CMAKE_VERSION_CHECK
 
-	echo "${cmake_version}" | awk "$(< "${cmake_version_check}")"
+	echo "${version}" | awk "$(< "${version_check_awk_script}")"
 }
 
 it_has_modern_ccache() {
@@ -84,7 +95,10 @@ it_has_modern_ccache() {
 }
 
 it_has_ninja_17() {
-	[[ "$(ninja --version)" = 1.7* ]]
+	local ninja_version
+	readonly ninja_version=$(ninja --version)
+
+	check_version_major_minor "${ninja_version}" 1 7
 }
 
 it_has_executables() {
